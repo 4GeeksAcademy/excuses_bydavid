21919d614fec93b1da23bf6c3b26e43a7d3d58bc
David Alonso <125155276+DavidAlonsoRod@users.noreply.github.com>
2024-06-14 18:26:45 +0000

    excuses

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#texto").innerHTML = generadorDeExcusas();
+  });
+  document.querySelector("#texto").innerHTML = generadorDeExcusas();
+};
+//write your code here
+let generadorDeExcusas = () => {
+  let quien = ["José Hernando", "Alberto", "Marcos", "Frodo"];
+  let accion = ["atropelló", "hackeo", "estrelló", "rompió"];
+  let que = ["mi portátil", "mi Imac", "mi móvil", "mi moto"];
+  let cuando = [
+    "antes de las clases",
+    "ayer",
+    "esta mañana",
+    "mientras echaba la siesta"
+  ];
+
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let queIndex = Math.floor(Math.random() * que.length);
+  let cuandoIndex = Math.floor(Math.random() * cuando.length);
+  return (
+    quien[quienIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    que[queIndex] +
+    " " +
+    cuando[cuandoIndex]
+  );
 };
